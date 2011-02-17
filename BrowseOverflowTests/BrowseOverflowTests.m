cfd3b943c41025dd3019a826218a9ba7ddafc509
Graham Lee <iamleeg@securemacprogramming.com>
2011-02-17 15:30:25 +0000

    Remove gratuitous failing test from the template code.

--- BrowseOverflowTests/BrowseOverflowTests.m
+++ BrowseOverflowTests/BrowseOverflowTests.m
@@ -11,23 +11,4 @@
 
 @implementation BrowseOverflowTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in BrowseOverflowTests");
-}
-
 @end
