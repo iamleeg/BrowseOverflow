3bcef6bb8629c34cbca0351159e66da2140e7580
Graham Lee <iamleeg@securemacprogramming.com>
2011-02-21 14:43:27 +0000

    Add a new test fixture for questions, and remove useless template code.

---
+++ BrowseOverflowTests/QuestionTests.h
@@ -0,0 +1,19 @@
+//
+//  QuestionTests.h
+//  BrowseOverflow
+//
+//  Created by Graham J Lee on 21/02/2011.
+//  Copyright 2011 Fuzzy Aliens Ltd. All rights reserved.
+//
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+//#import "application_headers" as required
+
+
+@interface QuestionTests : SenTestCase {
+    
+}
+
+@end
---
+++ BrowseOverflowTests/QuestionTests.m
@@ -0,0 +1,15 @@
+//
+//  QuestionTests.m
+//  BrowseOverflow
+//
+//  Created by Graham J Lee on 21/02/2011.
+//  Copyright 2011 Fuzzy Aliens Ltd. All rights reserved.
+//
+
+#import "QuestionTests.h"
+
+
+@implementation QuestionTests
+
+
+@end
