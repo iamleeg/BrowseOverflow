a6efb9903c41a9cf53bc3c79b436f4faee935c09
Graham Lee <iamleeg@securemacprogramming.com>
2011-02-21 12:51:20 +0000

    Probably worth providing some documentation. It's not like I'm writing a book on this or anything.

--- BrowseOverflow/Topic.h
+++ BrowseOverflow/Topic.h
@@ -8,15 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
-
+/**
+ * A class representing a particular subject on Stack Overflow.
+ */
 @interface Topic : NSObject {
     
 }
-
+/**
+ * A name for this topic, suitable for displaying in the UI.
+ */
 @property (readonly) NSString *name;
+/**
+ * The text with which questions on this topic get tagged on the website.
+ */
 @property (readonly) NSString *tag;
 
+/**
+ * Initialise a new instance of this class with a particular name and tag.
+ */
 - (id)initWithName:(NSString *)newName tag: (NSString *)tag;
+/**
+ * A list of questions recently asked on this topic.
+ */
 - (NSArray *)recentQuestions;
 
 @end
