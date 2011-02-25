a2c1a20f31cc6570eea2a5c590203c33e0c37f3c
Graham Lee <iamleeg@securemacprogramming.com>
2011-02-25 16:14:15 +0000

    Refactor Topic to do the question counting at addition time, rather than fetch time.

--- BrowseOverflow/Topic.m
+++ BrowseOverflow/Topic.m
@@ -9,6 +9,12 @@
 #import "Topic.h"
 #import "Question.h"
 
+@interface Topic ()
+
+- (NSArray *)sortQuestionsLatestFirst: (NSArray *)questionList;
+
+@end
+
 @implementation Topic
 @synthesize name;
 @synthesize tag;
@@ -31,12 +37,20 @@
 
 - (void)addQuestion: (Question *)question {
     NSArray *newQuestions = [questions arrayByAddingObject: question];
+    if ([newQuestions count] > 20) {
+        newQuestions = [self sortQuestionsLatestFirst: newQuestions];
+        newQuestions = [newQuestions subarrayWithRange: NSMakeRange(0, 20)];
+    }
     [questions release];
     questions = [newQuestions retain];
 }
 
 - (NSArray *)recentQuestions {
-    NSArray *sortedQuestions =  [questions sortedArrayUsingComparator: ^(id obj1, id obj2) {
+    return [self sortQuestionsLatestFirst: questions];
+}
+
+- (NSArray *)sortQuestionsLatestFirst: (NSArray *)questionList {
+    return [questionList sortedArrayUsingComparator: ^(id obj1, id obj2) {
         Question *q1 = (Question *)obj1;
         Question *q2 = (Question *)obj2;
         NSComparisonResult sortOrder = [q1.askedDate compare: q2.askedDate];
@@ -49,12 +63,6 @@
                 return NSOrderedSame;
         }
     }];
-    if ([sortedQuestions count] < 21) {
-        return sortedQuestions;
-    }
-    else {
-        return [sortedQuestions subarrayWithRange: NSMakeRange(0, 20)];
-    }
 }
 
 @end
