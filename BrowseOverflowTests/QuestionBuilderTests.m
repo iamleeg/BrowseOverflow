eec06ee2d77d5899766aea2fc9ea5144cf523342
Graham Lee <iamleeg@securemacprogramming.com>
2011-04-21 15:48:22 +0100

    Ensure that QuestionBuilder actually creates something.

--- BrowseOverflowTests/QuestionBuilderTests.m
+++ BrowseOverflowTests/QuestionBuilderTests.m
@@ -50,4 +50,45 @@
     STAssertEquals([error code], QuestionBuilderMissingDataError, @"This case should not be an invalid JSON error");
 }
 
+- (void)testJSONWithOneQuestionReturnsOneQuestionObject {
+    NSString *jsonString = @"{"
+    @"\"total\": 1,"
+    @"\"page\": 1,"
+    @"\"pagesize\": 30,"
+    @"\"questions\": ["
+    @"{"
+    @"\"tags\": ["
+    @"\"iphone\","
+    @"\"security\","
+    @"\"keychain\""
+    @"],"
+    @"\"answer_count\": 1,"
+    @"\"accepted_answer_id\": 3231900,"
+    @"\"favorite_count\": 1,"
+    @"\"question_timeline_url\": \"/questions/2817980/timeline\","
+    @"\"question_comments_url\": \"/questions/2817980/comments\","
+    @"\"question_answers_url\": \"/questions/2817980/answers\","
+    @"\"question_id\": 2817980,"
+    @"\"owner\": {"
+    @"\"user_id\": 23743,"
+    @"\"user_type\": \"registered\","
+    @"\"display_name\": \"Graham Lee\","
+    @"\"reputation\": 13459,"
+    @"\"email_hash\": \"563290c0c1b776a315b36e863b388a0c\""
+    @"},"
+    @"\"creation_date\": 1273660706,"
+    @"\"last_activity_date\": 1278965736,"
+    @"\"up_vote_count\": 2,"
+    @"\"down_vote_count\": 0,"
+    @"\"view_count\": 465,"
+    @"\"score\": 2,"
+    @"\"community_owned\": false,"
+    @"\"title\": \"Why does Keychain Services return the wrong keychain content?\""
+    @"}"
+    @"]"
+    @"}";
+    NSError *error = nil;
+    NSArray *questions = [questionBuilder questionsFromJSON: jsonString error: &error];
+    STAssertEquals([questions count], (NSUInteger)1, @"The builder should have created a question");
+}
 @end
