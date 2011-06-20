22ddd8577b29df5096259513728dd3511961eaef
Graham Lee <graham@fuzzyaliens.com>
2011-06-20 13:49:11 +0100

    Switch to NSJSONSerialization instead of SBJSON.

--- BrowseOverflow/QuestionBuilder.m
+++ BrowseOverflow/QuestionBuilder.m
@@ -7,7 +7,6 @@
 //
 
 #import "QuestionBuilder.h"
-#import "JSON.h"
 #import "Question.h"
 #import "Person.h"
 
@@ -15,10 +14,17 @@
 
 - (NSArray *)questionsFromJSON:(NSString *)objectNotation error:(NSError **)error {
     NSParameterAssert(objectNotation != nil);
-    NSDictionary *parsedObject = [objectNotation JSONValue];
+    NSData *unicodeNotation = [objectNotation dataUsingEncoding: NSUTF8StringEncoding];
+    NSError *localError = nil;
+    id jsonObject = [NSJSONSerialization JSONObjectWithData: unicodeNotation options: 0 error: &localError];
+    NSDictionary *parsedObject = (id)jsonObject;
     if (parsedObject == nil) {
         if (error != NULL) {
-            *error = [NSError errorWithDomain:QuestionBuilderErrorDomain code: QuestionBuilderInvalidJSONError userInfo: nil];
+            NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithCapacity: 1];
+            if (localError != nil) {
+                [userInfo setObject: localError forKey: NSUnderlyingErrorKey];
+            }
+            *error = [NSError errorWithDomain:QuestionBuilderErrorDomain code: QuestionBuilderInvalidJSONError userInfo: userInfo];
         }
         return nil;
     }
@@ -51,7 +57,11 @@
 - (void)fillInDetailsForQuestion:(Question *)question fromJSON:(NSString *)objectNotation {
     NSParameterAssert(question != nil);
     NSParameterAssert(objectNotation != nil);
-    NSDictionary *parsedObject = [objectNotation JSONValue];
+    NSData *unicodeNotation = [objectNotation dataUsingEncoding: NSUTF8StringEncoding];
+    NSDictionary *parsedObject = [NSJSONSerialization JSONObjectWithData: unicodeNotation options: 0 error: NULL];
+    if (![parsedObject isKindOfClass: [NSDictionary class]]) {
+        return;
+    }
     NSString *questionBody = [[[parsedObject objectForKey: @"questions"]  objectAtIndex: 0] objectForKey: @"body"];
     if (questionBody) {
         question.body = questionBody;
