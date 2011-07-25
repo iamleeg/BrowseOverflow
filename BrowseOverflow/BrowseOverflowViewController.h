0eb6cad26e4383cd20f8bedb13f8f9bac510933a
Graham Lee <iamleeg@securemacprogramming.com>
2011-07-25 12:15:43 +0100

    Create the new view controller subclass.

---
+++ BrowseOverflow/BrowseOverflowViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseOverflowViewController.h
+//  BrowseOverflow
+//
+//  Created by Graham Lee on 25/07/2011.
+//  Copyright 2011 Fuzzy Aliens Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseOverflowViewController : UIViewController
+
+@end
---
+++ BrowseOverflow/BrowseOverflowViewController.m
@@ -0,0 +1,51 @@
+//
+//  BrowseOverflowViewController.m
+//  BrowseOverflow
+//
+//  Created by Graham Lee on 25/07/2011.
+//  Copyright 2011 Fuzzy Aliens Ltd. All rights reserved.
+//
+
+#import "BrowseOverflowViewController.h"
+
+@implementation BrowseOverflowViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
