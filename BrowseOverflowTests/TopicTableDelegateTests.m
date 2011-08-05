0b20e71be5438351dd95f621f4482353085a8715
Graham Lee <iamleeg@securemacprogramming.com>
2011-08-05 12:19:58 +0100

    Switch this test to test TopicTableDataSource.

--- BrowseOverflowTests/TopicTableDelegateTests.m
+++ BrowseOverflowTests/TopicTableDelegateTests.m
@@ -7,7 +7,6 @@
 //
 
 #import "TopicTableDelegateTests.h"
-#import "TopicTableDelegate.h"
 #import "TopicTableDataSource.h"
 #import "Topic.h"
 
@@ -15,23 +14,19 @@
 {
     NSNotification *receivedNotification;
     TopicTableDataSource *dataSource;
-    TopicTableDelegate *delegate;
     Topic *iPhoneTopic;
 }
 
 - (void)setUp {
-    delegate = [[TopicTableDelegate alloc] init];
     dataSource = [[TopicTableDataSource alloc] init];
     iPhoneTopic = [[Topic alloc] initWithName: @"iPhone" tag: @"iphone"];
     [dataSource setTopics: [NSArray arrayWithObject: iPhoneTopic]];
-    delegate.tableDataSource = dataSource;
     [[NSNotificationCenter defaultCenter] addObserver: self selector: @selector(didReceiveNotification:) name: TopicTableDidSelectTopicNotification object: nil];
 }
 
 - (void)tearDown {
     receivedNotification = nil;
     dataSource = nil;
-    delegate = nil;
     iPhoneTopic = nil;
     [[NSNotificationCenter defaultCenter] removeObserver: self];
 }
@@ -42,7 +37,7 @@
 
 - (void)testDelegatePostsNotificationOnSelectionShowingWhichTopicWasSelected {
     NSIndexPath *selection = [NSIndexPath indexPathForRow: 0 inSection: 0];
-    [delegate tableView: nil didSelectRowAtIndexPath: selection];
+    [dataSource tableView: nil didSelectRowAtIndexPath: selection];
     STAssertEqualObjects([receivedNotification name], @"TopicTableDidSelectTopicNotification", @"The delegate should notify that a topic was selected");
     STAssertEqualObjects([receivedNotification object], iPhoneTopic, @"The notification should indicate which topic was selected");
 }
