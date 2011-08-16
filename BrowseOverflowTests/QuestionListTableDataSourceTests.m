61fcfe880fabbfed67c1ca4c89e7d164778d257c
Graham Lee <iamleeg@securemacprogramming.com>
2011-08-16 16:21:17 +0100

    Configure the question cell.

--- BrowseOverflowTests/QuestionListTableDataSourceTests.m
+++ BrowseOverflowTests/QuestionListTableDataSourceTests.m
@@ -10,6 +10,8 @@
 #import "QuestionListTableDataSource.h"
 #import "Topic.h"
 #import "Question.h"
+#import "Person.h"
+#import "QuestionSummaryCell.h"
 
 @implementation QuestionListTableDataSourceTests
 {
@@ -17,6 +19,7 @@
     Topic *iPhoneTopic;
     NSIndexPath *firstCell;
     Question *question1, *question2;
+    Person *asker1;
 }
 
 - (void)setUp {
@@ -26,9 +29,12 @@
     firstCell = [NSIndexPath indexPathForRow: 0 inSection: 0];
     question1 = [[Question alloc] init];
     question1.title = @"Question One";
+    question1.score = 2;
     question2 = [[Question alloc] init];
     question2.title = @"Question Two";
-
+    
+    asker1 = [[Person alloc] initWithName: @"Graham Lee" avatarLocation: @"http://www.gravatar.com/avatar/563290c0c1b776a315b36e863b388a0c"];
+    question1.asker = asker1;
 }
 
 - (void)tearDown {
@@ -37,6 +43,7 @@
     firstCell = nil;
     question1 = nil;
     question2 = nil;
+    asker1 = nil;
 }
 
 - (void)testTopicWithNoQuestionsLeadsToOneRowInTheTable {
@@ -59,4 +66,19 @@
     UITableViewCell *cell = [dataSource tableView: nil cellForRowAtIndexPath: firstCell];
     STAssertFalse([cell.textLabel.text isEqualToString: @"There was a problem connecting to the network."], @"Placeholder should only be shown when there's no content");
 }
+
+- (void)testQuestionCellIsOfCustomCellType {
+    [iPhoneTopic addQuestion: question1];
+    UITableViewCell *cell = [dataSource tableView: nil cellForRowAtIndexPath: firstCell];
+    STAssertTrue([cell isKindOfClass: [QuestionSummaryCell class]], @"Cells for displaying questions should be of a custom type");
+}
+
+- (void)testCellPropertiesAreTheSameAsTheQuestion {
+    [iPhoneTopic addQuestion: question1];
+    QuestionSummaryCell *cell = (QuestionSummaryCell *)[dataSource tableView: nil cellForRowAtIndexPath: firstCell];
+    STAssertEqualObjects(cell.titleLabel.text, @"Question One", @"Question cells display the question's title");
+    STAssertEqualObjects(cell.scoreLabel.text, @"2", @"Question cells display the question's score");
+    STAssertEqualObjects(cell.nameLabel.text, @"Graham Lee", @"Question cells display the asker's name");
+}
+
 @end
