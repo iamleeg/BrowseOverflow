a0100d91c64129eed2bca90b9ae283d02e294aea
Graham Lee <iamleeg@securemacprogramming.com>
2011-08-18 17:02:44 +0100

    Send notifications from the Avatar store.

--- BrowseOverflowTests/FakeNotificationCenter.m
+++ BrowseOverflowTests/FakeNotificationCenter.m
@@ -10,29 +10,46 @@
 
 
 @implementation FakeNotificationCenter
+{
+    NSMutableDictionary *observers;
+    NSMutableArray *notifications;
+}
 
 - (id)init {
     self = [super init];
     if (self) {
-        dictionary = [[NSMutableDictionary alloc] init];
+        observers = [[NSMutableDictionary alloc] init];
+        notifications = [[NSMutableArray alloc] init];
     }
     return self;
 }
 
 - (void)addObserver: (id)observer selector:(SEL)aSelector name:(NSString *)aName object:(id)anObject {
-    [dictionary setObject: observer forKey: aName];
+    [observers setObject: observer forKey: aName];
 }
 
 - (void)removeObserver:(id)observer {
-    [[dictionary copy]  enumerateKeysAndObjectsUsingBlock: ^(id key, id obj, BOOL *stop) {
+    [[observers copy]  enumerateKeysAndObjectsUsingBlock: ^(id key, id obj, BOOL *stop) {
         if ([obj isEqual: observer]) {
-            [dictionary removeObjectForKey: key];
+            [observers removeObjectForKey: key];
         }
     }];
 }
 
 - (BOOL)hasObject:(id)observer forNotification:(NSString *)aName {
-    return [[dictionary objectForKey: aName] isEqual: observer];
+    return [[observers objectForKey: aName] isEqual: observer];
 }
 
+- (void)postNotification:(NSNotification *)notification {
+    [notifications addObject: notification];
+}
+
+- (BOOL)didReceiveNotification:(NSString *)name fromObject:(id)obj {
+    for (NSNotification *note in notifications) {
+        if ([[note name] isEqualToString: name] && [[note object] isEqual: obj]) {
+            return YES;
+        }
+    }
+    return NO;
+}
 @end
